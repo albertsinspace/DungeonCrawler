77b90ab5814942a5ac522c90dcf6fee4791085b0
alber <alber@ALBPC>
2026-02-05 16:55:40 +0000

    Add project files.

---
+++ DungeonCrawler/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonCrawler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> weaponList = new List<string> {"sword", "gun", "crossbow", "bow", "magic"};
+
+            foreach (string weaponItem in weaponList)
+            {
+                Console.WriteLine(weaponItem);
+            }
+        }
+    }
+}
