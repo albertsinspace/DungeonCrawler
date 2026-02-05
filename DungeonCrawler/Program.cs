034e28969d9cc530a0410c2b8297eea418ff85ce
alber <alber>
2026-02-05 17:03:26 +0000

    Added menu

--- DungeonCrawler/Program.cs
+++ DungeonCrawler/Program.cs
@@ -16,6 +16,14 @@ namespace DungeonCrawler
             {
                 Console.WriteLine(weaponItem);
             }
+
+            Console.WriteLine("Dungeon Crawler");
+            Console.WriteLine();
+            Console.WriteLine("1. Play");
+            Console.WriteLine("2. How to play");
+            Console.WriteLine("3. Exit");
+            Console.WriteLine();
+
         }
     }
 }
